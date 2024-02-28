8ff9897e216f5ef4b3135735ebe50078d21165ca
Gowthamshegde <sgowtham327@gmail.com>
2024-02-28 12:47:29 +0530

    added date in 407

--- final/After_timeAdd.py
+++ final/After_timeAdd.py
@@ -404,7 +404,7 @@ def main(input):
                 if (proj_lead!= None and len(proj_lead) >0) and (PRF!= None and len(PRF) >0) :
                     if (last_pr_date!=None and last_pl_date!=None):
                         if (last_run_date_object<last_pr_date):
-                            if (last_pr_date==last_pl_date):
+                            if (last_pr_date.date()==last_pl_date.date()):
                                 values_760.append({"condition":1,"items":sap_items,"emp":prev_lead,"emp_type":"prev_lead"})
                                 called_count_760=called_count_760+1
                                 #calling 760 method to add
